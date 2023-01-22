c46072d41bf91b0b3edf4117501d11ecdd50b2e6
mohammed-lahbil <mohammedine19@gmail.com>
2023-01-22 22:26:03 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component: HomeComponent},
+  { path : 'about', component: AboutUsComponent},
+  { path : 'services', component: ServicesComponent},
+  { path : 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
