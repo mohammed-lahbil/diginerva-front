41b494e3604cb2a9918e4bd3a37408ec6686bf77
chorouk-elbojaddaini <choroukelbojaddaini001@gmail.com>
2023-08-11 22:08:38 +0100

    OurValues-PageAboutUs-Update

--- src/app/pages/about-us/about-us.component.spec.ts
+++ src/app/pages/about-us/about-us.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutUsComponent } from './about-us.component';
-
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 describe('AboutUsComponent', () => {
   let component: AboutUsComponent;
   let fixture: ComponentFixture<AboutUsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutUsComponent ]
+      declarations: [ AboutUsComponent ],
+      imports:[
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
+      ]
     })
     .compileComponents();
 
